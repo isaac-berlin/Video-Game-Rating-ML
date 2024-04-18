cb3eff1254f082529a65beccae7c06966f37df51
Andy Wang <wan00512@umn.edu>
2024-04-17 23:37:24 -0500

    added adaboosting using SAMME algorithm

--- src/ensemble_models.py
+++ src/ensemble_models.py
@@ -11,7 +11,7 @@ data.head()
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
@@ -32,23 +32,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 random_forest = RandomForestClassifier(random_state=42)
 bagging = BaggingClassifier(random_state=42)
 boosting = GradientBoostingClassifier(random_state=42)
+adaboostSAMME = AdaBoostClassifier(n_estimators=100, algorithm="SAMME",)
 
 # Train the models
 random_forest.fit(X_train, y_train)
 bagging.fit(X_train, y_train)
 boosting.fit(X_train, y_train)
+adaboostSAMME.fit(X_train, y_train)
 
 # Predictions
 rf_pred = random_forest.predict(X_test)
 bagging_pred = bagging.predict(X_test)
 boosting_pred = boosting.predict(X_test)
+adaboostSAMME_pred = adaboostSAMME.predict(X_test)
 
 # Evaluate the models
 rf_accuracy = accuracy_score(y_test, rf_pred)
 bagging_accuracy = accuracy_score(y_test, bagging_pred)
 boosting_accuracy = accuracy_score(y_test, boosting_pred)
+adaboostSAMME_accuracy = accuracy_score(y_test, adaboostSAMME_pred)
 
-rf_accuracy, bagging_accuracy, boosting_accuracy
+rf_accuracy, bagging_accuracy, boosting_accuracy, adaboostSAMME_accuracy
 print("")
 print("Random Forest Accuracy: ", rf_accuracy)
 print("Random Forest Recall: ", recall_score(y_test, rf_pred, average='weighted'))
@@ -65,6 +69,10 @@ print("Boosting Recall: ", recall_score(y_test, boosting_pred, average='weighted
 print("Boosting Precision: ", precision_score(y_test, boosting_pred, average='weighted'))
 print("Boosting F1 Score: ", f1_score(y_test, boosting_pred, average='weighted'), "\n")
 
+print("Adaptive Boosting SAMME Accuracy: ", adaboostSAMME_accuracy)
+print("Adaptive Boosting SAMME Recall: ", recall_score(y_test, adaboostSAMME_pred, average='weighted'))
+print("Adaptive Boosting SAMME Precision: ", precision_score(y_test, adaboostSAMME_pred, average='weighted'))
+print("Adaptive Boosting SAMME F1 Score: ", f1_score(y_test, adaboostSAMME_pred, average='weighted'), "\n")
 
 # Ensemble models on the test dataset
 
@@ -88,11 +96,13 @@ y_test_set = test_data['esrb_rating_encoded']
 rf_pred_test_set = random_forest.predict(X_test_set)
 bagging_pred_test_set = bagging.predict(X_test_set)
 boosting_pred_test_set = boosting.predict(X_test_set)
+adaboostSAMME_pred_test_set = adaboostSAMME.predict(X_test_set)
 
 # Evaluate the models on the test set
 rf_accuracy_test_set = accuracy_score(y_test_set, rf_pred_test_set)
 bagging_accuracy_test_set = accuracy_score(y_test_set, bagging_pred_test_set)
 boosting_accuracy_test_set = accuracy_score(y_test_set, boosting_pred_test_set)
+adaboostSAMME_accuracy_test_set = accuracy_score(y_test_set, adaboostSAMME_pred_test_set)
 
 # Performance metrics
 rf_recall_test_set = recall_score(y_test_set, rf_pred_test_set, average='weighted')
@@ -107,13 +117,17 @@ boosting_recall_test_set = recall_score(y_test_set, boosting_pred_test_set, aver
 boosting_precision_test_set = precision_score(y_test_set, boosting_pred_test_set, average='weighted')
 boosting_f1_test_set = f1_score(y_test_set, boosting_pred_test_set, average='weighted')
 
+adaboostSAMME_recall_test_set = recall_score(y_test_set, adaboostSAMME_pred_test_set, average='weighted')
+adaboostSAMME_precision_test_set = precision_score(y_test_set, adaboostSAMME_pred_test_set, average='weighted')
+adaboostSAMME_f1_test_set = f1_score(y_test_set, adaboostSAMME_pred_test_set, average='weighted')
+
 # Display the results
 results = {
-    "Model": ["Random Forest", "Bagging", "Boosting"],
-    "Accuracy": [rf_accuracy_test_set, bagging_accuracy_test_set, boosting_accuracy_test_set],
-    "Recall": [rf_recall_test_set, bagging_recall_test_set, boosting_recall_test_set],
-    "Precision": [rf_precision_test_set, bagging_precision_test_set, boosting_precision_test_set],
-    "F1 Score": [rf_f1_test_set, bagging_f1_test_set, boosting_f1_test_set]
+    "Model": ["Random Forest", "Bagging", "Boosting", "AdaBoostSAMME"],
+    "Accuracy": [rf_accuracy_test_set, bagging_accuracy_test_set, boosting_accuracy_test_set, adaboostSAMME_accuracy_test_set],
+    "Recall": [rf_recall_test_set, bagging_recall_test_set, boosting_recall_test_set, adaboostSAMME_recall_test_set],
+    "Precision": [rf_precision_test_set, bagging_precision_test_set, boosting_precision_test_set, adaboostSAMME_precision_test_set],
+    "F1 Score": [rf_f1_test_set, bagging_f1_test_set, boosting_f1_test_set, adaboostSAMME_f1_test_set]
 }
 
 print(pd.DataFrame(results))
